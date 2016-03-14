ef4cf85246be4864adc85c867bc9bb16024739d1
Nick Zelei <nickzelei@gmail.com>
2016-03-13 23:27:08 -0500

    added in call to menu that accidentally got deleted when adding to git

--- nomnom/food.py
+++ nomnom/food.py
@@ -5,6 +5,12 @@ import re
 
 @listen_to("what's for lunch today?", re.IGNORECASE)
 def hey(message):
+    menus = ubereats()
+    msg = ''
+    for menu in menus: msg += menu.fancyformat()
+    if msg == '':
+        msg = "Sorry, couldn't fetch the menus. Try again later"
+    message.send(msg)
     message.react('eggplant')
 
 @listen_to('hello foodbot!')
--- nomnom/scrape.py
+++ nomnom/scrape.py
@@ -8,7 +8,11 @@ class Menu:
         self.description = des
         self.dish = dish
         self.price = price
-
+    def fancyformat(self):
+        return self.restaurant + "\n" +\
+               self.dish + " - " + self.price + "\n" +\
+               self.description +\
+               "\n----------\n"
 
 def ubereats():
     data = {'city': 'CHI'}
@@ -16,7 +20,8 @@ def ubereats():
     response = requests.post('https://ubereats-city.appspot.com/_api/menus.get', headers=headers, data=json.dumps(data))
     jsonData = response.json()
 
-    timestamp = str(date.today() + timedelta(days=2)) + "T00:00:00"
+    #timestamp = str(date.today() + timedelta(days=2)) + "T00:00:00"
+    timestamp = str(date.today()) + "T00:00:00"
 
     menus = []
     for day in jsonData["menu"]:
